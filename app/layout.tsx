d225cdbc0f72963b8a39ca92ab94104662fca2e7
Hsen-123 <hazimehussein01@gmail.com>
2023-10-24 21:54:27 +0300

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { caveat } from "./font";
 import "./globals.css";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/Nav";
 import Footer from "./components/Footer";
 
 export const metadata: Metadata = {
