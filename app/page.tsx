0e88e15e53c74eaad67ecd6e7f36a7676a4872f3
Hsen-123 <hazimehussein01@gmail.com>
2023-10-24 22:13:58 +0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,3 @@
-import Hero from "./components/Hero";
-import IconsEveryWhere from "./components/IconsEveryWhere";
-import WhatWeDo from "./components/WhatWeDo/WhatWeDo";
-import LogoSlideShow from "./components/logoSlideShow/LogoSlideShow";
-
 export default function Home() {
-  return (
-    <div className="">
-      <Hero />
-      <LogoSlideShow />
-      <WhatWeDo />
-      <IconsEveryWhere />
-    </div>
-  );
+  return <div className="">hello</div>;
 }
