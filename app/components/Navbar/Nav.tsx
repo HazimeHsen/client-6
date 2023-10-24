f67340e0749400ceaf3bc68e3f79d73d5f9b9738
Hsen-123 <hazimehussein01@gmail.com>
2023-10-24 22:27:42 +0300

    .

--- app/components/Navbar/Nav.tsx
+++ app/components/Navbar/Nav.tsx
@@ -1,18 +1,9 @@
 "use client";
-import React, { useEffect } from "react";
-import { Navbar } from "@material-tailwind/react";
-import { motion, useAnimation } from "framer-motion";
 
 export default function Nav() {
-  const controls = useAnimation();
-
-  useEffect(() => {
-    controls.start("visible");
-  }, [controls]);
-
   return (
-    <Navbar className="relative bg-transparent mx-auto max-w-screen-xl px-8 py-5">
+    <div className="relative bg-transparent mx-auto max-w-screen-xl px-8 py-5">
       hello
-    </Navbar>
+    </div>
   );
 }
