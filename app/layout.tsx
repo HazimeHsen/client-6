741cb88dfff38649ac23444570345f984efaa177
Hsen-123 <hazimehussein01@gmail.com>
2023-10-24 20:51:10 +0300

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { caveat } from "./font";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -16,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={caveat.className}>
+      <body>
         <div className="min-h-screen overflow-hidden">
           <Navbar />
           {children}
