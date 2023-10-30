815b60a928a40b2c9066ede915271ed785b5cebc
Hsen-123 <hazimehussein01@gmail.com>
2023-10-30 20:36:35 +0200

    .

--- app/(pages)/flavors/page.tsx
+++ app/(pages)/flavors/page.tsx
@@ -77,9 +77,12 @@ const Flavors = () => {
             </div>
           </Animation>
           <div className="w-full mb-24 md:min-w-[300px] lg:min-w-[450px] md:text-start text-center">
-            <h1 className="text-2xl font-bold mb-2 text-customColor1 flex justify-center max-w-sm text-center uppercase mx-auto">
+            <Animation
+              animationType="fade-bottom"
+              startInView={false}
+              className="text-2xl font-bold mb-2 text-customColor1 flex justify-center max-w-sm text-center uppercase mx-auto">
               Meet the dessert that&rsquo;s causing a worldwide sensation
-            </h1>
+            </Animation>
           </div>
         </div>
         <div className="mx-auto flex flex-col justify-center items-center py-8">
@@ -89,8 +92,7 @@ const Flavors = () => {
               className="mx-auto flex my-10 md:flex-row md:gap-10 flex-col justify-center items-center h-full">
               <div>
                 <Animation
-                  startInView={false}
-                  animationType="width-increase"
+                  animationType="fade-bottom"
                   className="relative z-0 md:max-w-[300px] md:max-h-[300px] w-[250px] h-[250px] origin-center">
                   <Image
                     alt=""
@@ -100,25 +102,29 @@ const Flavors = () => {
                     height={200}
                   />
                 </Animation>
-                <div className="w-full  text-center">
+                <Animation
+                  animationType="fade-bottom"
+                  className="w-full  text-center">
                   <h1 className="text-2xl font-bold mb-2 text-customColor1 flex justify-center uppercase mx-auto">
                     {flavor.name}
                   </h1>
                   <p className="text-lg font-bold mb-2 text-customColor1 flex flex-wrap uppercase justify-center">
                     {flavor.description}
                   </p>
-                </div>
+                </Animation>
               </div>
-              <div className="flex flex-col min-w-[200px] bg-secondary rounded-lg p-3 font-semibold mt-4 text-gray-600">
+              <Animation
+                animationType="fade-bottom"
+                className="flex flex-col min-w-[200px] bg-secondary rounded-lg p-3 font-semibold mt-4 text-gray-600">
                 <h1 className="text-start text-xl">Contains: </h1>
-                <ul className="p-5 ">
+                <ul className="p-5 flex justify-center flex-col items-center">
                   {flavor.contains.map((contain, i) => (
                     <li className="list-disc w-fit " key={i}>
                       {contain}
                     </li>
                   ))}
                 </ul>
-              </div>
+              </Animation>
             </div>
           ))}
         </div>
